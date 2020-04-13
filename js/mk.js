55916261db91898c532e83284476cd85736fc04e
skak <1041159066@qq.com>
2020-04-13 09:12:15 +0800

    s

---
+++ js/mk.js
@@ -0,0 +1,9 @@
+    let data = Mock.mock('http://www.xuhan/blogs',{
+        "result|20":[{
+        "title":'@csentence',
+        "body":"@cparagraph",
+        "name":"@name",
+        "city":"@city",
+        "ctime":'@datetime("y-MM-dd HH:mm:ss")'
+    }]
+})
